20a6275df4500456045e018ddc3b944b4fcf4cac
LordBH <lordbh@ukr.net>
2016-03-30 23:13:26 +0300

    Create models GameRooms and UserConfigurations

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from app import db
+from datetime import datetime
+
+
+class GameRooms(db.Model):
+    __tablename__ = 'game_rooms'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    game_id = db.Column(db.String(80), nullable=False)
+    moves = db.Column(db.Text)
+    active = db.Column(db.DateTime, default=datetime.now())
+
+    def __init__(self):
+        pass
+
+
+class UserConfigurations(db.Model):
+    __tablename__ = 'user_config'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    nickname = db.Column(db.String(80), nullable=False)
+    quantity_to_win = db.Column(db.String(80), nullable=False)
+    board_size = db.Column(db.String(80), nullable=False)
+    selections = db.Column(db.String(80), nullable=False)
+    color = db.Column(db.String(80), nullable=False)
+    form = db.Column(db.Text)
+    last_active = db.Column(db.DateTime, default=datetime.now())
+
+    def __init__(self):
+        pass
+
+
+db.create_all()
