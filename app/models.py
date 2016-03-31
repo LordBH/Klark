da2025f587493f7c9e6263170540681dbc147bda
LordBH <lordbh@ukr.net>
2016-03-31 15:05:22 +0300

    Add new configurations on main page

--- app/models.py
+++ app/models.py
@@ -19,10 +19,10 @@ class UserConfigurations(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     nickname = db.Column(db.String(80), nullable=False)
-    quantity_to_win = db.Column(db.String(80), nullable=False)
-    board_size = db.Column(db.String(80), nullable=False)
-    selections = db.Column(db.String(80), nullable=False)
-    color = db.Column(db.String(80), nullable=False)
+    quantity_to_win = db.Column(db.Integer(20), nullable=False)
+    board_size = db.Column(db.Integer(20), nullable=False)
+    selections = db.Column(db.String(10), nullable=False)
+    color = db.Column(db.String(20), nullable=False)
     form = db.Column(db.Text)
     last_active = db.Column(db.DateTime, default=datetime.now())
 
