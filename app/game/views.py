abc9ae15c63bb888a19f362765fd496facdb27ad
LordBH <lordbh@ukr.net>
2016-04-01 19:14:45 +0300

    separate get and post methods into two func

--- app/game/views.py
+++ app/game/views.py
@@ -1,6 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
 
 game_blueprint = Blueprint('game', __name__, template_folder='templates',
                            static_folder='static', static_url_path='/%s' % __name__)
 
 extra = game_blueprint
+
+
+@extra.route(r'/<game>', methods=['GET', 'POST'])
+def play(game):
+    print()
+    print(game)
+    print()
+    return render_template('game/play.html')
