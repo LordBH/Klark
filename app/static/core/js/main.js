f5013ddc640dd50d9a1376c67eefcd98c3620c34
LordBH <lordbh@ukr.net>
2016-04-01 17:09:24 +0300

    create transtion opacity between menu-bar

--- app/static/core/js/main.js
+++ app/static/core/js/main.js
@@ -18,7 +18,7 @@ $('input[type=range]').on('mousemove', function (d) {
 });
 
 
-$('li>a').click(function (event) {
+$('li').click(function (event) {
     event.preventDefault();
     var c_name = event.currentTarget.id;
     var attributeTop = $('#' + c_name).offset().top;
@@ -26,4 +26,34 @@ $('li>a').click(function (event) {
         'top': attributeTop - 22,
         'transition': 'top .5s'
     });
+    ChangeFixedWrapper(c_name)
 });
+
+function ChangeFixedWrapper(id) {
+    if (id == 'games') {
+        showGames()
+    } else if (id == 'sett') {
+        showSettings()
+    }
+}
+function showGames() {
+    var c = 'board';
+    changeView(c);
+}
+
+function showSettings() {
+    var c = 'set-board';
+    changeView(c);
+}
+function showContacts() {
+    var c = 'cont-f';
+    changeView(c);
+}
+
+function changeView(ch) {
+    var show = 'show-div';
+    var wrapper = $('.' + show);
+
+    wrapper.removeClass(show);
+    $('.' + ch).parent().addClass(show);
+}
