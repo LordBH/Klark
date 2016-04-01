4b0d3f00e5eb8098b27b04064f4764768e5e0a85
LordBH <lordbh@ukr.net>
2016-04-02 01:44:51 +0300

    create user history games and add columns ip for config

--- app/models.py
+++ app/models.py
@@ -18,16 +18,28 @@ class UserConfigurations(db.Model):
     __tablename__ = 'user_config'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    ip = db.Column(db.String(15), nullable=False)
     nickname = db.Column(db.String(80), nullable=False)
     quantity_to_win = db.Column(db.Integer(20), nullable=False)
     board_size = db.Column(db.Integer(20), nullable=False)
     selections = db.Column(db.String(10), nullable=False)
     color = db.Column(db.String(20), nullable=False)
-    form = db.Column(db.Text)
+    symbols = db.Column(db.Text)
     last_active = db.Column(db.DateTime, default=datetime.now())
 
     def __init__(self):
         pass
 
+
+class UserGames(db.Model):
+    __tablename__ = 'played'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    ip = db.Column(db.String(15), nullable=False)
+    game_id = db.Column(db.String(80), nullable=False)
+    win = db.Column(db.Boolean)
+
+    def __init__(self):
+        pass
 
 db.create_all()
