18372b929a9bcaa5a5a8148236b39c436434249d
LordBH <lordbh@ukr.net>
2016-04-02 01:43:25 +0300

    Create changing board size and fixed-wrapper, separate fixed-wrapper,

--- app/static/core/js/main.js
+++ app/static/core/js/main.js
@@ -2,15 +2,27 @@
  * Created by lordbh on 3/30/16.
  */
 
+var boardRangeSize = 0;
+var maxSize = 0;
+var boardSymbols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'];
+
 $(function () {
     var list = $('input[type=range]');
-    var alert = $('.alert');
+    var alert = $('#alert');
     for (var i = 0; i < list.length; i++) {
         var idElement = '#' + list[i].id;
         var inputText = $(idElement).next('input[type=text]')[0];
         inputText.value = $(idElement)[0].value;
+
+        if (idElement == '#g-b-size') {
+            boardRangeSize = $(idElement)[0].value;
+            maxSize = $(idElement)[0].max;
+            createBoard(maxSize);
+            changeViewBoardSize(boardRangeSize);
+
+        }
     }
-    if (alert[0].innerHTML){
+    if (alert[0]) {
         alert.addClass('s-a');
         setTimeout(function () {
             alert.removeClass('s-a');
@@ -23,7 +35,13 @@ $(function () {
 $('input[type=range]').on('mousemove', function (d) {
     var idElement = '#' + d.target.id;
     var inputText = $(idElement).next('input[type=text]')[0];
-    inputText.value = $(idElement)[0].value;
+    var val = $(idElement)[0].value;
+    inputText.value = val;
+
+    if (idElement == '#g-b-size' && boardRangeSize != val) {
+        boardRangeSize = val;
+        changeViewBoardSize(val)
+    }
 });
 
 
@@ -51,8 +69,20 @@ function showGames() {
 }
 
 function showSettings() {
-    var c = 'set-board';
-    changeView(c);
+    var b = 'set-board';
+    changeView(b);
+
+
+    var hr = '.hor-set';
+    $('.wrapper').css('height', 1900);
+    $(hr).removeClass('no-animate-set');
+    $(hr).addClass('animate-set');
+    //
+    var config = '.config';
+    $(config).show();
+    $(config).removeClass('no-animate-set');
+    $(config).addClass('animate-set');
+
 }
 function showContacts() {
     var c = 'cont-f';
@@ -62,11 +92,66 @@ function showContacts() {
 function changeView(ch) {
     var show = 'show-div';
     var wrapper = $('.' + show);
+    if (wrapper.children().hasClass('set-board')) {
+        var hr = '.hor-set';
+        $('.wrapper').css('height', '100%');
+        $(hr).removeClass('animate-set');
+        $(hr).addClass('no-animate-set');
+
+        var config = '.config';
+        $(config).addClass('no-animate-set');
+        $(config).removeClass('animate-set');
+        $(config).hide();
+    }
 
     wrapper.removeClass(show);
     $('.' + ch).parent().addClass(show);
 }
 
-function sendRecall(){
+function sendRecall() {
     $('#re-call').submit();
+}
+
+function saveSettings() {
+    $('#settings').submit();
+}
+
+function createBoard(val) {
+    var bor = '.bor';
+    var black = 'blc';
+    var white = 'wht';
+    var v = parseInt(val);
+
+    for (var y = 0; y < v; y++)
+        for (var i = 1; i < v + 1; i++) {
+            if (i % 2 == 0) {
+                $(bor).append('<div id="' + boardSymbols[y] + i + '" class="dsp-none ' + white + '"></div>');
+                if (v % 2 == 0 && val == i) {
+                    var extra = black;
+                    black = white;
+                    white = extra;
+                }
+            } else {
+                $(bor).append('<div id="' + boardSymbols[y] + i + '" class="dsp-none ' + black + '"></div>');
+            }
+        }
+}
+
+function changeViewBoardSize(val) {
+    var v = parseInt(val);
+    $('.bor>div').css({
+       'flex-grow': v,
+       'flex-basis': 100/v + '%',
+        'width': 650/(v*v),
+        'height': 650/v
+    });
+
+    for (var i = 0; i < maxSize; i++)
+        for (var j = 1; j < maxSize + 1; j++) {
+            if (j < v + 1 && i < v)
+                $('#' + boardSymbols[i] + j).removeClass('dsp-none');
+            else
+                $('#' + boardSymbols[i] + j).addClass('dsp-none');
+}
+
 }
