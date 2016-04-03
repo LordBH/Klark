f48726040d373e1df387f6fc377831bda27e1d43
LordBH <lordbh@ukr.net>
2016-04-03 10:14:05 +0300

    configurate mail settting

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from config import DevelopmentConfig
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 
 
 # application
@@ -12,6 +13,9 @@ app.config.from_object(DevelopmentConfig)
 
 # DataBase
 db = SQLAlchemy(app)
+
+# Mail
+mail = Mail(app)
 
 # Web sockets
 socket_io = SocketIO(app)
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from os import path
+from os import path, urandom
 
 
 BASE_DIR = path.join(path.dirname(__file__))
@@ -7,13 +7,22 @@ DB_NAME = "klark.db"
 
 class ConfigClass:
     # App Settings
-    SECRET_KEY = 'SECRET_KEY'
+    SECRET_KEY = urandom(60)
 
     # Database
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     # create DB on absolute path
     SQLALCHEMY_DATABASE_URI = "sqlite:////" + BASE_DIR + '/' + DB_NAME
     CSRF_ENABLED = True
+
+    # Flask-Mail settings
+    MAIL_USERNAME = 'testingdjangomaxx@gmail.com'
+    MAIL_PASSWORD = '][poi123'
+    MAIL_DEFAULT_SENDER = 'testingdjangomaxx@gmail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    AUTHOR_EMAIL = 'testingdjangomaxx@gmail.com'
 
     # Flask-User settings
     STATIC_FOLDER = 'static'
