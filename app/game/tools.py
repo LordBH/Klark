06e075c8cc6f83bebb68ef8f2b94643c4ef1232e
LordBH <lordbh@ukr.net>
2016-04-04 12:45:31 +0300

    add functions which responding for loading and creating game

--- app/game/tools.py
+++ app/game/tools.py
@@ -1,13 +1,40 @@
-from flask import abort
+from app.models import db
+from flask import abort, session, request
 
 
 def check_game(game):
     if len(game) != 8:
         abort(404)
-
     try:
-        game = int(game)
+        int(game)
     except ValueError:
-        abort(403)
+        abort(404)
 
 
+def set_player(q):
+    nickname = session.get('nickname')
+    ip = request.remote_addr
+
+    q.player2 = nickname
+    q.ip2 = ip
+
+    db.session.add(q)
+    db.session.commit()
+
+
+def create_context_game(query):
+    nickname = session.get('nickname')
+    context = {
+        'link': request.host + query.game_id,
+        'moves': 0,
+        'quantity': query.q_count,
+        'permissions': 0,
+    }
+    if nickname == query.player1:
+        context['you'] = query.player1,
+        context['opponent'] = query.player2
+    else:
+        context['you'] = query.player2,
+        context['opponent'] = query.player1
+
+    return context
--- app/game/views.py
+++ app/game/views.py
@@ -1,21 +1,60 @@
-from flask import Blueprint, render_template
-from app.game.tools import check_game
-# from flask_socketio import join_room
-# from app.models import GameRooms, UserConfigurations
+from app import socket_io, db
+from app.game.tools import check_game, set_player, create_context_game
+from app.models import GameRooms
+from flask import Blueprint, render_template, abort
+from flask_socketio import join_room, emit
 
 
 game_blueprint = Blueprint('game', __name__, template_folder='templates',
                            static_folder='static', static_url_path='/%s' % __name__)
 
 extra = game_blueprint
+KLARK_ROOM = 'KLARK'
+GAME_CREATED = 'game-created'
 
 
 @extra.route(r'/<game>', methods=['GET', 'POST'])
 def play(game):
+    check_game(game)
+    q = GameRooms.query.filter_by(game_id=game).first()
+    if q is None:
+        abort(404)
+    context = create_context_game(q)
+    return render_template('game/play.html', context=context)
 
-    exist = check_game(game)
 
-    if exist:
-        pass
+@socket_io.on('join-rooms', namespace='/core')
+def first_connect(data):
 
-    return render_template('game/play.html')
+    join_room(KLARK_ROOM)
+
+
+@socket_io.on('create-game', namespace='/core')
+def create_game(info):
+
+    q = GameRooms(info, create=True)
+    db.session.add(q)
+    db.session.commit()
+    info['game'] = q.game_id
+
+    join_room(info['game'])
+
+    emit(GAME_CREATED, info, room=KLARK_ROOM)
+
+
+@socket_io.on('prepare-game', namespace='/core')
+def loading_second_player(data):
+    game_id = data.get('game')
+    ex = {
+        'msg': 'no game find',
+        'game': game_id
+    }
+    q = GameRooms.query.filter_by(game_id=game_id).first()
+
+    if game_id and q:
+        join_room(game_id)
+        set_player(q)
+        ex['msg'] = 'Game Find'
+
+        return emit('load-game', ex, room=game_id)  # info game
+    emit('load-game', ex)  # info game
