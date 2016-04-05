85d53524572f5f2875b9abb57a9713db4b98bc38
LordBH <lordbh@ukr.net>
2016-04-05 10:41:40 +0300

    create function for loading context for game board

--- app/game/tools.py
+++ app/game/tools.py
@@ -1,5 +1,31 @@
-from app.models import db
-from flask import abort, session, request
+from app.models import UserConfigurations, db
+from app.core.tools import get_colors
+from flask import abort, request, session
+from random import choice
+
+
+GAME_CONTEXT_CHECKER = {}
+
+
+def create_template(q):
+    game = q.game_id
+    opponent = q.player1
+    board_size = q.q_size
+    quantity = q.q_count
+    rules = ''
+    for y in q.rules.split('|')[:-1]:
+        rules += y.upper() + ' '
+    butt = """<td class="start-play" onclick="startPlay(event)">Play</td>"""
+
+    template = ''
+    template += butt
+
+    for x in [game, opponent, board_size, quantity, rules]:
+        template += "<td>" + str(x) + "</td>"
+
+    # template += butt
+
+    return template
 
 
 def check_game(game):
@@ -11,11 +37,10 @@ def check_game(game):
         abort(404)
 
 
-def set_player(q):
-    nickname = session.get('nickname')
+def set_player(q, pl):
     ip = request.remote_addr
 
-    q.player2 = nickname
+    q.player2 = pl
     q.ip2 = ip
 
     db.session.add(q)
@@ -23,18 +48,43 @@ def set_player(q):
 
 
 def create_context_game(query):
-    nickname = session.get('nickname')
     context = {
-        'link': request.host + query.game_id,
+        'link': 'http://' + request.host + '/' + query.game_id,
         'moves': 0,
         'quantity': query.q_count,
-        'permissions': 0,
+        'size': query.q_size,
+        'player1': query.player1,
+        'player2': query.player2,
+        'rules': {}
     }
-    if nickname == query.player1:
-        context['you'] = query.player1,
-        context['opponent'] = query.player2
-    else:
-        context['you'] = query.player2,
-        context['opponent'] = query.player1
+    GAME_CONTEXT_CHECKER.setdefault(query.game_id, {'count': 0, 'flag': choice([True, False])})
+    game_id = GAME_CONTEXT_CHECKER[query.game_id]
+    if game_id['count'] == 0:
+        context['flag'] = game_id['flag']
+    elif game_id['count'] == 1:
+        context['flag'] = not game_id['flag']
+    game_id['count'] += 1
+
+    for x in query.rules.split('|')[:-1]:
+        val = False
+        if x:
+            val = True
+        context['rules'][x.upper()] = val
+    if session.get('nickname') != 'Guest':
+        get_user_settings(context)
 
     return context
+
+
+def get_user_settings(cont):
+    q = UserConfigurations.query.filter_by(ip=request.remote_addr).first()
+    if q is not None:
+        col = get_colors(q.__dict__)['colors']
+        for y in col:
+            cont[y] = '#'
+            for x in ['r', 'g', 'b']:
+                s = hex(int(col[y][x]))[2:]
+                if len(s) != 2:
+                    s = '0' + s
+                cont[y] += s
+        cont['symbols'] = q.symbols
--- app/game/views.py
+++ app/game/views.py
@@ -1,7 +1,7 @@
 from app import socket_io, db
-from app.game.tools import check_game, set_player, create_context_game
+from app.game.tools import check_game, set_player, create_context_game, create_template
 from app.models import GameRooms
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, session
 from flask_socketio import join_room, emit
 
 
@@ -9,8 +9,9 @@ game_blueprint = Blueprint('game', __name__, template_folder='templates',
                            static_folder='static', static_url_path='/%s' % __name__)
 
 extra = game_blueprint
-KLARK_ROOM = 'KLARK'
+KLARK_ROOM = 'klark-room'
 GAME_CREATED = 'game-created'
+GAME_ROOMS = {}
 
 
 @extra.route(r'/<game>', methods=['GET', 'POST'])
@@ -23,38 +24,67 @@ def play(game):
     return render_template('game/play.html', context=context)
 
 
-@socket_io.on('join-rooms', namespace='/core')
+@socket_io.on('joining', namespace='/core')
 def first_connect(data):
-
-    join_room(KLARK_ROOM)
+    for x in data['rooms']:
+        join_room(str(x))
 
 
 @socket_io.on('create-game', namespace='/core')
-def create_game(info):
+def create_game(data):
+    q = GameRooms(data, create=True)
 
-    q = GameRooms(info, create=True)
+    data['template'] = create_template(q)
+    data['game'] = q.game_id
+
     db.session.add(q)
     db.session.commit()
-    info['game'] = q.game_id
 
-    join_room(info['game'])
+    emit(GAME_CREATED, data, room=KLARK_ROOM)
 
-    emit(GAME_CREATED, info, room=KLARK_ROOM)
+    emit('my-game-created', data)
 
 
 @socket_io.on('prepare-game', namespace='/core')
 def loading_second_player(data):
     game_id = data.get('game')
+    player2 = data.get('nickname')
     ex = {
         'msg': 'no game find',
         'game': game_id
     }
     q = GameRooms.query.filter_by(game_id=game_id).first()
-
     if game_id and q:
-        join_room(game_id)
-        set_player(q)
+        set_player(q, player2)
         ex['msg'] = 'Game Find'
 
-        return emit('load-game', ex, room=game_id)  # info game
     emit('load-game', ex)  # info game
+    emit('load-game', ex, room=game_id)
+
+
+@socket_io.on('game-on', namespace='/core')
+def loading_players(data):
+    game_id = data.get('rooms')
+    if game_id is not None:
+        join_room(game_id)
+        GAME_ROOMS.setdefault(game_id, set())
+        GAME_ROOMS[game_id].add(session.get('nickname'))
+        return emit('players', len(GAME_ROOMS[game_id]), room=KLARK_ROOM)
+
+
+@socket_io.on('enter-message', namespace='/core')
+def send_message(ex):
+    data = {
+        'msg': ex.get('msg'),
+        'nickname': session.setdefault('nickname', 'Guest'),
+    }
+    return emit('show-message', data, room=ex.get('room'))
+
+
+@socket_io.on('symbol-set', namespace='/core')
+def send_message(ex):
+    data = {
+        'id': ex.get('id'),
+        'symbol': ex.get('symbol'),
+    }
+    return emit('show-symbol', data, room=ex.get('room'))
