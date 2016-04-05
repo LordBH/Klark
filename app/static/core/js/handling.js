de0aa0a2b7576a2204dcf75b513de66a97141390
LordBH <lordbh@ukr.net>
2016-04-05 10:38:05 +0300

    fix problem with joing to game

--- app/static/core/js/handling.js
+++ app/static/core/js/handling.js
@@ -1,26 +1,44 @@
 var socket = io.connect('http://' + document.domain + ':' + location.port + '/core');
-var game = 0;
-var change = '';
+var gameChoose = '';
 
-sendSocket('join-rooms');
 
 function sendSocket(emitName, obj) {
     socket.emit(emitName, obj);
-
 }
+
+sendSocket('joining', {rooms: ['klark-room']});
 
 socket.on('game-created', function (data) {
     Advertise(data);
-    change = data['game'];
 });
+
+socket.on('my-game-created', function (data) {
+        sendSocket('joining', {rooms: [data.game]});
+});
+
+
 
 socket.on('load-game', function (data) {
     console.log(data);
-    console.log('start');
-    setTimeout(function () {
-        window.location.href = 'http://127.0.0.1:5000/' + change
-    }, 5000);
+    console.log('loading-game');
+    // if (gameChoose = data['game']) {
+        setTimeout(function () {
+            window.location.href = 'http://' + document.domain + ':' + location.port + '/' + data['game']
+        }, 1000);
+    // }
 });
+
+
+function Advertise(context) {
+    console.log('Advertise');
+    var trAttr = '.wall-games>tbody>tr';
+    var idElement = $(trAttr).last();
+    idElement = parseInt(idElement.attr('id')) + 1;
+    
+    $('.wall-games>tbody').append(
+        "<tr id='" + idElement + "'>" + context.template + "</tr>"
+    );
+}
 
 $('.out-a>.in-a').on('click', function () {
     var data = {
@@ -32,30 +50,24 @@ $('.out-a>.in-a').on('click', function () {
         'd': $('input[name=ch-d]')[0].checked
     };
 
+    console.log('create-game');
     console.log(data);
     sendSocket('create-game', data);
 
 });
 
+function startPlay(event) {
+    console.log('start preparing');
+    var parentId = event.target.parentNode.id;
+    var children = $('#' + parentId).children('td');
+    var nickname = $('#nickname').val();
+    var data = {
+        'game' : children[1].innerHTML,
+        'nickname': nickname
+    };
 
-// $('.out-a>.in-f').on('click', function (){
-//     var data = {
-//         'nickname': $('#nickname').val(),
-//         'q': $('#q-to-win').val(),
-//         'size': $('#g-b-size').val(),
-//         'v': $('input[name=ch-v]').val(),
-//         'h': $('input[name=ch-h]').val(),
-//         'd': $('input[name=ch-d]').val()
-//     };
-//
-//     sendSocket('create-game', data);
-//
-// });
-
-
-function Advertise(context) {
-    alertMessage(context);
-    sendSocket('prepare-game', {'game': context['game']});
-    console.log(context);
+    sendSocket('joining', {rooms: [data.game]});
+    setTimeout(function () {
+        sendSocket('prepare-game', data);
+    }, 2000)
 }
-
